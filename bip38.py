c97d15176c5b4710742360c19f01d8055bc48a17
nformant <54002590+nformant1@users.noreply.github.com>
2022-03-31 17:32:30 +0200

    Update bip38.py

--- bip38.py
+++ bip38.py
@@ -9,21 +9,17 @@ import binascii
 import base58
 
 def bip38_encrypt(privkey,passphrase):
-    #print("lets encrypt something")
     '''BIP0038 non-ec-multiply encryption. Returns BIP0038 encrypted privkey.'''
     privformat = get_privkey_format(privkey)
-    #print (privformat)
     if privformat in ['wif_compressed','hex_compressed']:
         compressed = True
         flagbyte = b'\xe0'
-        #print ("i am compressed")
         if privformat == 'wif_compressed':
             privkey = encode_privkey(privkey,'hex_compressed')
             privformat = get_privkey_format(privkey)
     if privformat in ['wif', 'hex']:
         compressed = False
         flagbyte = b'\xc0'
-        #print ("i am UNcompressed")
     if privformat == 'wif':
         privkey = encode_privkey(privkey,'hex')
         privformat = get_privkey_format(privkey)
@@ -31,20 +27,11 @@ def bip38_encrypt(privkey,passphrase):
     pubkey = privtopub(privkey)
     addr = pubtoaddr(pubkey)
 
-    #print ("pubkey")
-    #print (pubkey)
-
-    #print ("addr")
-    #print (addr)
-    
     #print ("hashlib")
     #print (hashlib.sha256(hashlib.sha256(addr.encode('utf-8')).digest()).hexdigest())
 
 
     addresshash = hashlib.sha256(hashlib.sha256(addr.encode('utf-8')).digest()).digest()[0:4]
-
-    #print("addresshash")
-    #print(addresshash)
 
     #addresshash2 = hashlib.sha256(hashlib.sha256(addr.encode('utf-8')).digest()).hexdigest()
     #addresshash2 = addresshash2[0:8]
@@ -58,26 +45,13 @@ def bip38_encrypt(privkey,passphrase):
     #print (hashed_key)
 
     derivedhalf1 = key[0:32]
-    #print("derivedhalf1")
-    #print(binascii.hexlify(derivedhalf1))
-
     derivedhalf2 = key[32:64]
 
-    #print("derivedhalf2")
-    #print(binascii.hexlify(derivedhalf2))
+
     aes = AES.new(derivedhalf2, 1) # from python2 /Crypto/Cipher/AES.py Default is `MODE_ECB`.
-    #print(aes)
     encryptedhalf1 = aes.encrypt(binascii.unhexlify('%0.32x' % (int(privkey[0:32], 16) ^ int(binascii.hexlify(derivedhalf1[0:16]), 16))))
-    #encryptedhalf1 = aes.encrypt(binascii.unhexlify('%0.32x' % (long(privkey[0:32], 16) ^ long(binascii.hexlify(derivedhalf1[0:16]), 16))))
-    
+     
     encryptedhalf2 = aes.encrypt(binascii.unhexlify('%0.32x' % (int(privkey[32:64], 16) ^ int(binascii.hexlify(derivedhalf1[16:32]), 16))))
-    #print ("flagbyte:       " + str(type(flagbyte)))
-    #print (flagbyte)
-    #print ("addresshash:    " + str(type(addresshash)))
-    #print ("encryptedhalf1: " + str(type(encryptedhalf1)))
-    #print (binascii.hexlify(encryptedhalf1))
-    #print ("encryptedhalf2: " + str(type(encryptedhalf2)))
-    #print (encryptedhalf2)
     encrypted_privkey = (b'\x01\x42' + flagbyte + addresshash + encryptedhalf1 + encryptedhalf2)
     encrypted_privkey += hashlib.sha256(hashlib.sha256(encrypted_privkey).digest()).digest()[:4] # b58check for encrypted privkey
     encrypted_privkey = base58.b58encode(encrypted_privkey)
@@ -90,17 +64,6 @@ def bip38_decrypt(encrypted_privkey,passphrase):
     # Documentation from https://en.bitcoin.it/wiki/BIP_0038
 
     # 1. Collect encrypted private key and passphrase from user.
-    print (encrypted_privkey)
-    print (passphrase)
-
-    unencoded_string = str(bytearray.fromhex( encrypted_privkey ))
-    encoded_string= base58.b58encode(unencoded_string)
-
-    print (encoded_string)
-    
-    print (encrypted_privkey.encode('utf_8'))
-    print (base58.b58decode(encrypted_privkey))
-
     d = base58.b58decode(encrypted_privkey.encode('utf_8'))
     d = d[2:]
     flagbyte = d[0:1]
@@ -114,16 +77,17 @@ def bip38_decrypt(encrypted_privkey,passphrase):
     addresshash = d[0:4]
 
 
-    print ("addresshash: " + str(addresshash))
+    #print ("addresshash: " + str(addresshash))
     d = d[4:-4]
 
     # 3. Derive decryption key for seedb using scrypt with passpoint, addresshash, and ownerentropy
     key = scrypt.hash(passphrase,addresshash, 16384, 8, 8)
     derivedhalf1 = key[0:32]
     derivedhalf2 = key[32:64]
+
     encryptedhalf1 = d[0:16]
     encryptedhalf2 = d[16:32]
-    aes = AES.new(derivedhalf2, 9) # If in doubt, use ``MODE_EAX``.
+    aes = AES.new(derivedhalf2, 1) # from python2 /Crypto/Cipher/AES.py Default is `MODE_ECB`.
     
     # 4. Decrypt encryptedpart2 using AES256Decrypt to yield the last 8 bytes of seedb and the last 8 bytes of encryptedpart1.
     decryptedhalf2 = aes.decrypt(encryptedhalf2)
@@ -131,17 +95,10 @@ def bip38_decrypt(encrypted_privkey,passphrase):
     decryptedhalf1 = aes.decrypt(encryptedhalf1)
 
 
-    #print ("decryptedhalf")
-    #print(decryptedhalf1)
-    #print(decryptedhalf2)
-    #print ("priv 1")
     priv = decryptedhalf1 + decryptedhalf2
-    #print ("priv 2")
-    #print (priv)
 
     # 7. Multiply passfactor by factorb mod N to yield the private key associated with generatedaddress.
     priv = binascii.unhexlify('%064x' % (int(binascii.hexlify(priv), 16) ^ int(binascii.hexlify(derivedhalf1), 16)))
-    #print (priv)
     pub = privtopub(priv)
     if compressed:
         pub = encode_pubkey(pub,'hex_compressed')
@@ -151,14 +108,13 @@ def bip38_decrypt(encrypted_privkey,passphrase):
 
     # 8. Convert that private key into a Bitcoin address, honoring the compression preference specified in the encrypted key.
     addr = pubtoaddr(pub)
-    print (addr)
-    print (wif)
+
 
     # 9. Hash the Bitcoin address, and verify that addresshash from the encrypted private key record matches the hash. If not, report that the passphrase entry was incorrect.
     if hashlib.sha256(hashlib.sha256(addr.encode('utf-8')).digest()).digest()[0:4] != addresshash:
         print('Verification failed. Password is incorrect.')
-        print(hashlib.sha256(hashlib.sha256(addr.encode('utf-8')).digest()).digest()[0:4])
-        print(addresshash)
+        #print(hashlib.sha256(hashlib.sha256(addr.encode('utf-8')).digest()).digest()[0:4])
+        #print(addresshash)
         return ''
     else:
         return wif
